cab576b191cae9aec69b4da289c5dc9b94440ec8
João Costa <joao.pmc20@gmail.com>
2021-06-23 15:28:27 +0100

    Add entity models

--- SolarCoffee.Data/Models/Customer.cs
+++ SolarCoffee.Data/Models/Customer.cs
@@ -6,13 +6,13 @@ namespace SolarCoffee.Data.Models
     public class Customer
     {
         public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
         [MaxLength(100)]
         public string FirstName { get; set; }
         [MaxLength(100)]
         public string LastName { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public DateTime UpdatedOn { get; set; }
-        
+
         public CustomerAddress PrimaryAddress { get; set; }
     }
 }
